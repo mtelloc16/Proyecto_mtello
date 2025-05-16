8ea971ea3ff747ed778dcb3ea28034f7abee6d27
Tello Maria Fernanda <mtello@generali.com.ec>
2025-05-16 12:53:03 -0500

    test demoblaze actualizado

--- test_demoblaze.py
+++ test_demoblaze.py
@@ -12,58 +12,53 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://www.demoblaze.com")
 time.sleep(2)
 # Ir a la categoría "Laptops"
-categoria_laptops = driver.find_element(By.LINK_TEXT, "Laptops")
-categoria_laptops.click()
+driver.find_element(By.LINK_TEXT, "Laptops").click()
 time.sleep(2)
 # Seleccionar "MacBook Pro"
-producto1 = driver.find_element(By.XPATH, "//a[text()='MacBook Pro']")
-producto1.click()
+driver.find_element(By.LINK_TEXT, "MacBook Pro").click()
 time.sleep(2)
 # Agregar al carrito
-boton_agregar1 = driver.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg']")
-boton_agregar1.click()
+driver.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg']").click()
 time.sleep(2)
-# Aceptar la alerta de agregar al carrito
 driver.switch_to.alert.accept()
 time.sleep(2)
-# Volver a la página principal y seleccionar otro producto
+# Volver al inicio
 driver.get("https://www.demoblaze.com")
 time.sleep(2)
+# Ir a la categoría "Phones"
+driver.find_element(By.LINK_TEXT, "Phones").click()
+time.sleep(2)
 # Seleccionar "Nokia lumia 1520"
-producto2 = driver.find_element(By.XPATH, "//a[text()='Nokia lumia 1520']")
-producto2.click()
+driver.find_element(By.LINK_TEXT, "Nokia lumia 1520").click()
 time.sleep(2)
 # Agregar al carrito
-boton_agregar2 = driver.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg']")
-boton_agregar2.click()
+driver.find_element(By.XPATH, "//a[@class='btn btn-success btn-lg']").click()
 time.sleep(2)
-# Aceptar la alerta
 driver.switch_to.alert.accept()
 time.sleep(2)
 # Ir al carrito
-carrito = driver.find_element(By.ID, "cartur")
-carrito.click()
+driver.find_element(By.ID, "cartur").click()
 time.sleep(2)
 # Iniciar la compra
-boton_comprar = driver.find_element(By.XPATH, "//button[@class='btn btn-success']")
-boton_comprar.click()
+driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
 time.sleep(2)
-# Esperar hasta que el formulario esté visible
-wait = WebDriverWait(driver, 20)
-wait.until(EC.presence_of_element_located((By.ID, "name")))
-# Completar formulario con datos
+# Esperar que aparezca el formulario
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.ID, "name")))
+# Llenar el formulario
 driver.find_element(By.ID, "name").send_keys("María Fernanda")
 driver.find_element(By.ID, "country").send_keys("Ecuador")
 driver.find_element(By.ID, "city").send_keys("Quito")
 driver.find_element(By.ID, "card").send_keys("1234 5678 9101 1121")
 driver.find_element(By.ID, "month").send_keys("12")
 driver.find_element(By.ID, "year").send_keys("2025")
-# Esperar y hacer scroll hasta el botón 'Purchase'
-boton_confirmar = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Purchase']")))
-driver.execute_script("arguments[0].scrollIntoView(true);", boton_confirmar)
-boton_confirmar.click()
-time.sleep(3)
-# Confirmación de compra realizada
-print("Compra realizada con éxito.")
+# Confirmar la compra
+boton_purchase = driver.find_element(By.XPATH, "//button[text()='Purchase']")
+boton_purchase.click()
+time.sleep(2)
+# Confirmar mensaje de éxito (opcional)
+mensaje = driver.find_element(By.CLASS_NAME, "sweet-alert").text
+print("Mensaje de confirmación:")
+print(mensaje)
 # Cerrar el navegador
 driver.quit()
